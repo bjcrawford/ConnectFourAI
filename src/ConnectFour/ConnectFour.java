840ca676c25c8bbd6e5744c5dc49b91458ace1e5
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-29 15:44:03 -0400

    Minor changes to main for testing previous commits

--- src/ConnectFour/ConnectFour.java
+++ src/ConnectFour/ConnectFour.java
@@ -10,19 +10,25 @@ import javax.swing.SwingUtilities;
  */
 public class ConnectFour {
     
+    static ConnectFourBoard myBoard;
+    
     public static void main(String args[]) {
+        
+        myBoard = new ConnectFourBoard(8, 8);
         
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
             }
         });
+        
+        System.out.println(myBoard);
     }
     
     private static void createAndShowGUI() {
         JFrame gui = new JFrame("Connect Four AI");
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.add(new welcomePanel());
+        gui.add(new welcomePanel(myBoard));
         gui.pack();
         gui.setVisible(true);
     }
