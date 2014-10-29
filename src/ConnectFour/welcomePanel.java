b0dc8651c51e5ea18ff89ded8f1fc736af0c89e8
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-29 15:42:12 -0400

    welcomePanel class changes: Removed drawBoard method, added ConnectFourBoard as member variable

--- src/ConnectFour/welcomePanel.java
+++ src/ConnectFour/welcomePanel.java
@@ -17,7 +17,10 @@ import javax.swing.JPanel;
  */
 public class welcomePanel extends JPanel {
     
-    public welcomePanel() {
+    ConnectFourBoard myBoard;
+    
+    public welcomePanel(ConnectFourBoard board) {
+        this.myBoard = board;
         setBorder(BorderFactory.createLineBorder(Color.black));
     }
     
@@ -29,49 +32,6 @@ public class welcomePanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        paintBoard(g, 20, 20, 8, 8);
-    }
-    
-    public void paintBoard(Graphics g, int x, int y, int rows, int cols) {
-        
-        int bgWidth = 200;
-        int bgHeight = 200;
-        int borderWidth = 10;
-        int boardWidth = bgWidth - (2 * borderWidth);
-        int boardHeight = bgHeight - (2 * borderWidth);
-        
-        
-        System.out.println("bgWidth: " + bgWidth + "\n");
-        System.out.println("bgHieght: " + bgHeight + "\n");
-        System.out.println("borderWidth: " + borderWidth + "\n");
-        System.out.println("boardWidth: " + boardWidth + "\n");
-        System.out.println("boardHeight: " + boardHeight + "\n");
-        
-        g.setColor(Color.GRAY);
-        g.fillRect(x, y, bgWidth, bgHeight);
-        g.setColor(Color.BLACK);
-        g.drawRect(x, y, bgWidth, bgHeight);
-        g.setColor(Color.YELLOW);
-        g.fillRect(x + borderWidth,
-                   y + borderWidth,
-                   boardWidth, 
-                   boardHeight);
-        
-        int rowSep = (boardWidth - borderWidth)/rows;
-        int colSep = (boardWidth - borderWidth)/cols;
-        
-        System.out.println("rowSep: " + rowSep + "\n");
-        System.out.println("colSep: " + colSep + "\n");
-        
-        g.setColor(Color.GRAY);
-        for(int i = y + 2 * borderWidth; i < (y + boardHeight); i = i + rowSep)
-        {
-            
-            for(int j = x + 2 * borderWidth; j < (x + boardHeight); j = j + colSep)
-            {
-                System.out.println("i: " + i + "; j: " + j + "\n");
-                g.fillOval(j, i, rowSep - 10, colSep - 10);
-            }
-        }
+        myBoard.drawBoard(g, 20, 20);
     }
 }
