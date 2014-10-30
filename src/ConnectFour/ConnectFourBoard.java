eb927e01eb6db9b5536b72d41927ca8b32e00799
Robert Burns <tud05819@temple.edu>
2014-10-30 19:34:26 -0400

    fixed mistake of mine

--- src/ConnectFour/ConnectFourBoard.java
+++ src/ConnectFour/ConnectFourBoard.java
@@ -121,6 +121,7 @@ public class ConnectFourBoard {
                     else
                         pieceColor--;
                     
+                    
                     break;
                 }
             }
