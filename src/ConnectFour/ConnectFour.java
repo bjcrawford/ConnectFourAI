2320f5911ebfaf01bbf33f17b2dc0c825720f1b1
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-29 22:12:50 -0400

    Clean up and testing

--- src/ConnectFour/ConnectFour.java
+++ src/ConnectFour/ConnectFour.java
@@ -11,25 +11,58 @@ import javax.swing.SwingUtilities;
 public class ConnectFour {
     
     static ConnectFourBoard myBoard;
+    static JFrame gui;
+    static WelcomePanel wp;
     
     public static void main(String args[]) {
         
         myBoard = new ConnectFourBoard(8, 8);
+        gui = new JFrame("Connect Four AI");
+        wp = new WelcomePanel(myBoard);
         
         SwingUtilities.invokeLater(new Runnable() {
+            @Override
             public void run() {
-                createAndShowGUI();
+                initGUI();
             }
         });
         
+        /*
+        for(int i = 0; i < 10; i++)
+        {
+            if(!myBoard.insertPiece(1, 4))
+                System.out.println("Cannot insert piece\n");
+        }
+        */
+        
         System.out.println(myBoard);
+        
+        myBoard.insertPiece(2, 3);
+        myBoard.insertPiece(2, 3);
+        myBoard.insertPiece(2, 3);
+        myBoard.insertPiece(2, 2);
+        myBoard.insertPiece(2, 2);
+        myBoard.insertPiece(2, 1);
+        
+        myBoard.insertPiece(1, 0);
+        myBoard.insertPiece(1, 1);
+        myBoard.insertPiece(1, 2);
+        myBoard.insertPiece(1, 3);
+        
+        System.out.println(myBoard);
+        
+        int win = myBoard.checkWin();
+        if(win == 1)
+            System.out.println("Red Wins!\n");
+        else if(win == 2)
+            System.out.println("Black wins!\n");
+        else
+            System.out.println("No winner\n");
     }
     
-    private static void createAndShowGUI() {
-        JFrame gui = new JFrame("Connect Four AI");
+    private static void initGUI() {
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        welcomePanel WP = new welcomePanel(myBoard);
-        gui.add(WP);
+        gui.add(wp);
         gui.pack();
         gui.setVisible(true);
         
