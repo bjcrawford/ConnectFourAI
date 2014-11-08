ed52ccc2d02917732596a57c18a15eb1daec2282
Brett Crawford <brettjamescrawford@gmail.com>
2014-11-08 18:53:37 -0500

    Removed unused import

--- src/ConnectFour/EndPanel.java
+++ src/ConnectFour/EndPanel.java
@@ -5,7 +5,6 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
