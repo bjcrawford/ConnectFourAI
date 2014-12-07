5a41a12eeb3afa63ffe1558b1a462cbdcd953b14
Brett Crawford <brettjamescrawford@gmail.com>
2014-12-06 21:04:35 -0500

    Changes to the AI tester

--- src/ConnectFour/AIHeadToHead.java
+++ src/ConnectFour/AIHeadToHead.java
@@ -4,7 +4,7 @@ package ConnectFour;
 public class AIHeadToHead {
     
     public static void main(String args[]) {
-        playGames(1);
+        playGames(3000);
     }
     
     public static void playGames(int numOfGames) {
@@ -24,60 +24,61 @@ public class AIHeadToHead {
             i = 1;
             win = 0;
             CFB.resetBoard();
-            AI1 = new AIMonteCarlo1Player(1, 20000, CFB);
-            AI2 = new AIMonteCarlo2Player(2, 20000, CFB);
+            AI1 = new AIMinimax1Player(1, 4, CFB);
+            AI2 = new AIMinimax1Player(2, 4, CFB);
 
             while(win == 0)
             {
-                System.out.println("\nGame Board - Turn " + i + "\n");
-                System.out.println(CFB);
+                //System.out.println("\nGame Board - Turn " + i + "\n");
+                //System.out.println(CFB);
                 int col;
                 if(i % 2 == 1)
                 {
-                    System.out.println("Red Turn\n");
-                    long start = System.currentTimeMillis();
+                    //System.out.println("Red Turn\n");
+                    //long start = System.currentTimeMillis();
                     col = AI1.getNextMove();
-                    long end = System.currentTimeMillis();
-                    System.out.println("Move took " + (end-start) + " ms to compute");
+                    //long end = System.currentTimeMillis();
+                    //System.out.println("Move took " + (end-start) + " ms to compute");
                     CFB.insertPiece(1, col);
-                    System.out.println("\nRed inserts in column " + (col+1));
+                    //System.out.println("\nRed inserts in column " + (col+1));
                 }
                 else
                 {
-                    System.out.println("Black Turn\n");
-                    long start = System.currentTimeMillis();
+                    //System.out.println("Black Turn\n");
+                    //long start = System.currentTimeMillis();
                     col = AI2.getNextMove();
-                    long end = System.currentTimeMillis();
-                    System.out.println("Move took " + (end-start) + " ms to compute");
+                    //long end = System.currentTimeMillis();
+                    //System.out.println("Move took " + (end-start) + " ms to compute");
                     CFB.insertPiece(2, col);
-                    System.out.println("\nBlack inserts in column " + (col+1));
+                    //System.out.println("\nBlack inserts in column " + (col+1));
                 }
                 i++;
                 win = CFB.checkWin();
             }
 
-            System.out.println("\nGame Board - Turn " + i);
-            System.out.println(CFB);
+            //System.out.println("\nGame Board - Turn " + i);
+            //System.out.println(CFB);
             if(win == 1)
             {
-                System.out.println("Red Wins!");
+                //System.out.println("Red Wins!");
                 redWins++;
             }
             else if(win == 2)
             {
-                System.out.println("Black Wins!");
+                //System.out.println("Black Wins!");
                 blackWins++;
             }
             else
             {
-                System.out.println("Draw!");
+                //System.out.println("Draw!");
                 draws++;
             }
             games++;
+            System.out.println("\n");
+            System.out.println("Out of " + games + " games played: ");
+            System.out.println("  Red wins: " + redWins);
+            System.out.println("  Black wins: " + blackWins);
+            System.out.println("  Draws: " + draws);
         }
-        System.out.println("Out of " + numOfGames + " games played: ");
-        System.out.println("Red wins: " + redWins);
-        System.out.println("Black wins: " + blackWins);
-        System.out.println("Draws: " + draws);
     }
 }
